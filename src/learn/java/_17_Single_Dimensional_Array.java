3c327c7bd80dcf802a727a915eec57aba00f76be
VishalJadhavQA <vishaljadhav.career@gmail.com>
2023-07-04 18:15:58 +0530

    Java Basics programs

---
+++ src/learn/java/_17_Single_Dimensional_Array.java
@@ -0,0 +1,35 @@
+package learn.java;
+
+public class _17_Single_Dimensional_Array {
+	
+	public static void main(String[] args) {
+	
+	int[] a = new int[3]; // Declare and create array
+	
+	a[0] = 2;		//Assigning values to array
+	a[1] = 4;
+	a[2] = 6;
+	
+	
+	
+	System.out.println(a[0]); // Accessing values of array
+	System.out.println(a[1]);
+	System.out.println(a[2]);
+		
+
+	System.out.println("*-*-*-*--*-*-*-*-*-*-*-*-*-*-*");
+	
+	/* Single Dimensional Array Shortcut */
+	
+	int[] b = {5,3,9};
+	
+	System.out.println(b[0]);
+	System.out.println(b[1]);
+	System.out.println(b[2]);
+	
+	
+	
+	
+
+	}
+}
