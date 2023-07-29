e8ae81dfeca2101c323aaba1849b4cf7159e5545
VishalJadhavQA <vishaljadhav.career@gmail.com>
2023-07-29 13:39:34 +0530

    Java Basics programs

---
+++ src/learn/java/_23_Wrapper_Classes.java
@@ -0,0 +1,32 @@
+package learn.java;
+
+public class _23_Wrapper_Classes {
+
+	public static void main(String[] args) {
+		
+		/* converting the primitive data types into objects */
+		
+		int a = 7;
+		
+		Integer obj;
+		
+		obj = a;
+		
+		System.out.println(a); // Printing Primitive data type
+
+		System.out.println(obj); // Printing object
+
+		
+		/* objects into primitive data types */ 
+		
+		Integer obj1 = 5;
+		
+		int b = obj1;
+		 
+		System.out.println(obj1); // Printing object
+		System.out.println(b); // Printing Primitive data type
+		
+
+	}
+
+}
